34f0979b3e6d112f6c693d61564c16c237e6c3af
Børge Lanes <borge@lanes-data.no>
2014-01-16 21:49:49 +0100

    Initial commit

---
+++ vim-bootstrap.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+
+echoerr() {
+    echo "$@" 1>&2
+}
+while true; do
+    echo "This script will erase your .vim directory and .vimrc file."
+    read -p "Will you really continue?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+    esac
+done
+
+command -v git
+if [ $? -ne 0 ]; then
+    echoerr "git command not found"
+    exit 1
+fi
+
+OLDDIR=$( pwd )
+cd ~
+rm -rf -- .vim
+git clone https://github.com/leinz/vim-config.git .vim
+cd .vim
+git submodule update --init
+
+cd ~
+echo ":so ~/.vim/vimrc" > .vimrc
+
+cd $OLDDIR
+
+echo "\nCommand-T instructions:"
+echo "\t- have ruby with dev headers installed (version 1.8 preferred)"
+echo "\t- cd to ~/.vim/bundle/command-t/ruby/command-t"
+echo "\t- run ruby extconf.rb && make"
+echo "\t- Your vim must be compiled with ruby support"
+echo "\t- On OSX: Homebrew with MacVim"
+echo "\t- On Ubuntu: vim-nox"
+echo "\nHappy vimming!"
